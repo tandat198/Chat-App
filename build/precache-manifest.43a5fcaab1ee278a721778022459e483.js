e353bda354897ae124886667f8e7ff5f01237eec
Dat Nguyen <tandat198@gmail.com>
2020-04-13 17:58:20 +0700

    add build

---
+++ build/precache-manifest.43a5fcaab1ee278a721778022459e483.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6039dd040c173c4d7f1d37ec06a3599",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1b73b0cbd38710d4f3b7",
+    "url": "/static/css/main.16d8d1a0.chunk.css"
+  },
+  {
+    "revision": "d2193acfee86af9d2ec0",
+    "url": "/static/js/2.46173ee0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.46173ee0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b73b0cbd38710d4f3b7",
+    "url": "/static/js/main.6aee8641.chunk.js"
+  },
+  {
+    "revision": "262ca1cd662d0bbcdabe",
+    "url": "/static/js/runtime-main.1f8d7041.js"
+  },
+  {
+    "revision": "02452b0e283e07e47b74c4439a8a2dc0",
+    "url": "/static/media/gear.02452b0e.svg"
+  },
+  {
+    "revision": "03a0ce3127ffb57ba2c05543577e537f",
+    "url": "/static/media/group.03a0ce31.svg"
+  },
+  {
+    "revision": "385cf06efdd12eff3637cd916f5808a8",
+    "url": "/static/media/login.385cf06e.jpg"
+  },
+  {
+    "revision": "7d7c0ba2e6cdbb43e6fdc5d8aa10f8f5",
+    "url": "/static/media/logout.7d7c0ba2.svg"
+  },
+  {
+    "revision": "874cd32ba7b10b335bfb0b9faa517940",
+    "url": "/static/media/more.874cd32b.svg"
+  },
+  {
+    "revision": "6421649f4264892fe83c7ad9ce3169ae",
+    "url": "/static/media/plus.6421649f.svg"
+  },
+  {
+    "revision": "515ea9eaf0d612a466fcaa6e6f440631",
+    "url": "/static/media/spinner.515ea9ea.svg"
+  },
+  {
+    "revision": "7753ba0867cf47b9e9525cc557641a99",
+    "url": "/static/media/user.7753ba08.svg"
+  }
+]);
